0cbc94408e2cf755207442d1947e8ce4d74e2b9e
satyabratait <satyabrata@itobuz.com>
2023-03-10 12:45:59 +0530

    first commit

---
+++ math.mjs
@@ -0,0 +1,20 @@
+export function add(a,b) {
+    return a + b;
+};
+
+export function sub(a,b) {
+    if (a>b) {
+        return a-b;
+    }
+    else{
+        return b-a;
+    }
+}
+
+export function multiply(a,b) {
+    return a * b;
+}
+
+export function div(a,b) {
+    return a / b;
+}
