705726be53a8455e85beba608832b9e3281a0dfb
satyabratait <satyabrata@itobuz.com>
2023-03-10 12:40:04 +0530

    Third commit

---
+++ modules/database.mjs
@@ -0,0 +1,292 @@
+//weatherlocation database
+export let weatherdata =
+  // module.exports
+  [
+    {
+      id: 1,
+      location: {
+        country: "India",
+        name: "Bhubaneswar",
+        region: "Orissa",
+      },
+      current: {
+        tempC: 29,
+        tempF: 84.2,
+        humidity: 84,
+        cloud: 0,
+        condition: {
+          text: "Mist",
+        },
+      },
+    },
+
+    {
+      id: 2,
+      location: {
+        country: "United Kingdom",
+        name: "London",
+        region: "City of London, Greater London",
+      },
+      current: {
+        tempC: 4,
+        tempF: 39.2,
+        humidity: 75,
+        cloud: 100,
+        condition: {
+          text: "Overcast",
+        },
+      },
+    },
+
+    {
+      id: 3,
+      location: {
+        country: "India",
+        name: "New Delhi",
+        region: "Delhi",
+      },
+      current: {
+        tempC: 22,
+        tempF: 71.6,
+        humidity: 65,
+        cloud: 0,
+        condition: {
+          text: "Overcast",
+        },
+      },
+    },
+
+    {
+      id: 4,
+      location: {
+        country: "United States of America",
+        name: "New York",
+        region: "New York",
+      },
+      current: {
+        tempC: 4.4,
+        tempF: 39.9,
+        humidity: 57,
+        cloud: 0,
+        condition: {
+          text: "Clear",
+        },
+      },
+    },
+
+    {
+      id: 5,
+      location: {
+        country: "France",
+        name: "Paris",
+        region: "Ile-de-France",
+      },
+      current: {
+        tempC: 1,
+        tempF: 33.8,
+        humidity: 93,
+        cloud: 75,
+        condition: {
+          text: "Fog",
+        },
+      },
+    },
+
+    {
+      id: 6,
+      location: {
+        country: "Norway",
+        name: "Oslo",
+        region: "Oslo",
+      },
+      current: {
+        tempC: -4,
+        tempF: 24.8,
+        humidity: 86,
+        cloud: 75,
+        condition: {
+          text: "Partly cloudy",
+        },
+      },
+    },
+
+    {
+      id: 7,
+      location: {
+        country: "Norway",
+        name: "Oslo",
+        region: "Oslo",
+      },
+      current: {
+        tempC: -4,
+        tempF: 24.8,
+        humidity: 86,
+        cloud: 75,
+        condition: {
+          text: "Partly cloudy",
+        },
+      },
+    },
+
+    {
+      id: 8,
+      location: {
+        country: "India",
+        name: "Srinagar",
+        region: "Jammu and Kashmir",
+      },
+      current: {
+        tempC: 8.7,
+        tempF: 47.7,
+        humidity: 76,
+        cloud: 27,
+        condition: {
+          text: "Partly cloudy",
+        },
+      },
+    },
+
+    {
+      id: 9,
+      location: {
+        country: "United States of America",
+        name: "Chicago",
+        region: "Illinois",
+      },
+      current: {
+        tempC: 1,
+        tempF: 33.8,
+        humidity: 82,
+        cloud: 0,
+        condition: {
+          text: "Clear",
+        },
+      },
+    },
+
+    {
+      id: 10,
+      location: {
+        country: "Turkey",
+        name: "Istanbul",
+        region: "Istanbul",
+      },
+      current: {
+        tempC: 7,
+        tempF: 44.6,
+        humidity: 81,
+        cloud: 100,
+        condition: {
+          text: "Overcast",
+        },
+      },
+    },
+
+    {
+      id: 11,
+      location: {
+        country: "Russia",
+        name: "Moscow",
+        region: "Moscow City",
+      },
+      current: {
+        tempC: 0,
+        tempF: 32,
+        humidity: 86,
+        cloud: 100,
+        condition: {
+          text: "Snow showers",
+        },
+      },
+    },
+
+    {
+      id: 12,
+      location: {
+        country: "Israel",
+        name: "Tel Aviv-Yafo",
+        region: "Tel Aviv",
+      },
+      current: {
+        tempC: 18,
+        tempF: 64.4,
+        humidity: 77,
+        cloud: 75,
+        condition: {
+          text: "Partly cloudy",
+        },
+      },
+    },
+
+    {
+      id: 13,
+      location: {
+        country: "United Arab Emirates",
+        name: "Dubai",
+        region: "Dubai",
+      },
+      current: {
+        tempC: 24,
+        tempF: 75.2,
+        humidity: 78,
+        cloud: 0,
+        condition: {
+          text: "Sunny",
+        },
+      },
+    },
+
+    {
+      id: 14,
+      location: {
+        country: "Germany",
+        name: "Berlin",
+        region: "Berlin",
+      },
+      current: {
+        tempC: -1,
+        tempF: 30.2,
+        humidity: 93,
+        cloud: 100,
+        condition: {
+          text: "Mist",
+        },
+      },
+    },
+
+    {
+      id: 15,
+      location: {
+        country: "Switzerland",
+        name: "Grindelwald",
+        region: "",
+      },
+      current: {
+        tempC: -3,
+        tempF: 26.6,
+        humidity: 100,
+        cloud: 50,
+        condition: {
+          text: "Partly cloudy",
+        },
+      },
+    },
+
+    {
+      id: 16,
+      location: {
+        country: "India",
+        name: "Kolkata",
+        region: "West Bengal",
+      },
+      current: {
+        tempC: 35,
+        tempF: 95,
+        humidity: 25,
+        cloud: 0,
+        condition: {
+          text: "Mist",
+        },
+      },
+    },
+  ];
---
+++ modules/functions.mjs
@@ -0,0 +1,248 @@
+import { weatherdata } from "./database.mjs";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+let filepath = "./database.txt",encoding ="utf8";
+
+let obj = {
+  id: 17,
+  location: {
+    country: "",
+    name: "",
+    region: "",
+  },
+  current: {
+    tempC: null,
+    tempF: null,
+    humidity: null,
+    cloud: null,
+    condition: {
+      text: "",
+    },
+  },
+};
+
+//findlocation
+export function findLocation(locationInfo) {
+  let data = readfile(filepath,encoding);
+  const result = data.find(
+    (item) => item.location.name.toLowerCase() == locationInfo.toLowerCase()
+  );
+  console.log(result);
+}
+// findLocation('oslo');
+
+//addlocation
+export function addLocation(
+  loc,
+  region,
+  country,
+  tempC,
+  tempF,
+  humidity,
+  cloud,
+  text
+) {
+  let data = readfile(filepath,encoding);
+
+  let i = data.length;
+  if (
+    data.find((item) => item.location.name.toLowerCase() != loc.toLowerCase())
+  ) {
+    i += 1;
+    obj.id = i;
+    console.log(obj.id);
+    obj.location.name = loc;
+    obj.location.region = region;
+    obj.location.country = country;
+    obj.current.tempC = tempC;
+    obj.current.tempF = tempF;
+    obj.current.humidity = humidity;
+    obj.current.cloud = cloud;
+    obj.current.condition.text = text;
+
+    data.push(obj);
+    writefile(filepath,data);
+    console.log(data);
+  } else {
+    console.log("location exists");
+  }
+}
+// addLocation('bengaluru','karnataka','india',34,93,25,100,'cloudy');
+
+//deletelocation
+export function deleteLocation(locationInfo) {
+  let data = readfile(filepath,encoding);
+  let index = data.findIndex(
+    (item) => item.location.name.toLowerCase() == locationInfo.toLowerCase()
+  );
+  if (index > -1) {
+    data.splice(index, 1);
+    writefile(filepath,data);
+    console.log(data);
+  } else {
+    console.log("location not found");
+  }
+}
+// deleteLocation('bengaluru')
+
+//allcities
+export function allCities() {
+  const data = readfile(filepath,encoding);
+  data.forEach((element) => {
+    console.log(element.location.name);
+  });
+}
+// allCities();
+
+//read the txt file
+function readfile(filepath,encoding) {
+  try {
+    const data = fs.readFileSync(path.resolve(__dirname,filepath), encoding);
+    const obj = JSON.parse(data);
+    return obj;
+  } catch (err) {
+    console.log(err);
+  }
+                                                                                                                                    
+}
+
+
+//write in file
+function writefile(filepath,data) {
+    fs.writeFileSync(path.resolve(__dirname,filepath), JSON.stringify(data), (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+}
+  
+  //updatelocation
+  // export function updatetempcLocation(locationInfo) {
+  //     let updatearray = locationInfo.split(" ");
+  //     let index = weatherdata.findIndex(
+  //       (item) => item.location.name.toLowerCase() == updatearray[0].toLowerCase()
+  //     );
+  //     if (index > -1) {
+  //       weatherdata[index].current.tempC = updatearray[1];
+  //     } else {
+  //       console.log("location not found");
+  //     }
+  //   }
+  
+  //   export function updatetempfLocation(locationInfo) {
+  //     let updatearray = locationInfo.split(" ");
+  //     let index = weatherdata.findIndex(
+  //       (item) => item.location.name.toLowerCase() == updatearray[0].toLowerCase()
+  //     );
+  //     if (index > -1) {
+  //       weatherdata[index].current.tempF = updatearray[1];
+  //     } else {
+  //       console.log("location not found");
+  //     }
+  //   }
+  
+  //   export function updatehumidityLocation(locationInfo) {
+  //     let updatearray = locationInfo.split(" ");
+  //     let index = weatherdata.findIndex(
+  //       (item) => item.location.name.toLowerCase() == updatearray[0].toLowerCase()
+  //     );
+  //     if (index > -1) {
+  //       weatherdata[index].current.humidity = updatearray[1];
+  //     } else {
+  //       console.log("location exists");
+  //     }
+  //   }
+  
+  //   export function updatecloudLocation(locationInfo) {
+  //     let updatearray = locationInfo.split(" ");
+  //     let index = weatherdata.findIndex(
+  //       (item) => item.location.name.toLowerCase() == updatearray[0].toLowerCase()
+  //     );
+  //     if (index > -1) {
+  //       weatherdata[index].current.cloud = updatearray[1];
+  //     } else {
+  //       console.log("location exists");
+  //     }
+  //   }
+  
+  //   export function updatetextLocation(locationInfo) {
+  //     let updatearray = locationInfo.split(" ");
+  //     let index = weatherdata.findIndex(
+  //       (item) => item.location.name.toLowerCase() == updatearray[0].toLowerCase()
+  //     );
+  //     if (index > -1) {
+  //       weatherdata[index].current.condition.text = updatearray[1];
+  //     } else {
+  //       console.log("location exists");
+  //     }
+  //   }
+  
+  //file system
+  // console.log(obj.toString());
+  // fs.appendFile('./database.mjs',JSON.stringify(obj),err =>{
+  //     console.log(1);
+  //     if (err) {
+  //         console.log(err);
+  //     }
+  // });
+  // fs.readFile('./database.mjs','utf8', (err, data) => {
+  //     if (err) {
+  //         console.error(err);
+  //         return;
+  //       }
+  //       console.log(data.toString());
+  // });
+  // fs.writeFile('./database.mjs',weatherdata.toString(), err => {
+  //     if (err) {
+  //         console.error(err);
+  //       }
+  // })
+  // fs.readFile('./database.mjs', function(err, data) {
+  // fs.appendFile('./database.mjs',obj.toString(),err =>{
+  //     console.log(1);
+  //     if (err) {
+  //         console.log(err);
+  //     }
+  //     console.log(weatherdata);
+  // });
+  //   });
+  
+  // console.log(index);
+  
+  // function readfile() {
+  //   // fs.readFile('./database.txt','utf8', (err, data) => {
+  //   //     if (err) {
+  //   //         console.error(err);
+  //   //         return;
+  //   //       }
+  //   //   content = util.format(data);
+  //   // performTask(data);
+  //   //let obj = JSON.parse(data);
+  //   //return obj;
+  //   //console.log(obj);
+  //   try {
+  //     const data = fs.readFileSync("./database.txt", "utf8");
+  //     const obj = JSON.parse(data);
+  //     return obj;
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  //   // let obj = JSON.parse(data);
+  //   // console.log(obj);
+  //   //   console.log(data`                                                                                                                                                                        `);
+  // }
+  
+  // function performTask(content){
+  //     const obj = JSON.parse(content)
+  //     // console.log(obj);
+  //     console.log(obj.find((item) => item.id == 4));
+  //     // console.log(typeof content);
+  
+  // }
+  
